82d8ba7e1266b48600d8570db9a1f28fa419ed02
Fabrizzio <fbrzzochoa@gmail.com>
2026-03-16 20:41:22 -0500

    Implement double linked list for inventory management

--- sistema_almacen/Codigo1/InventarioListaDoble.java
+++ sistema_almacen/Codigo1/InventarioListaDoble.java
@@ -1,1 +1,23 @@
+public class InventarioListaDoble {
+    private Nodo cabeza;
+    private Nodo cola;
 
+    public void agregarAlFinal(Producto p) {
+        Nodo nuevo = new Nodo(p);
+        if (cabeza == null) {
+            cabeza = cola = nuevo;
+        } else {
+            cola.siguiente = nuevo;
+            nuevo.anterior = cola;
+            cola = nuevo;
+        }
+    }
+
+    public Nodo getCabeza() { 
+    	return cabeza; 
+    }
+    
+    public boolean estaVacio() { 
+    	return cabeza == null; 
+    }
+}
