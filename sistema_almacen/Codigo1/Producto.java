80a92b769464e8681136b8bf39e4cbbbfb4b8af0
Fabrizzio <fbrzzochoa@gmail.com>
2026-03-16 20:40:19 -0500

    Add Producto class with properties and methods

--- sistema_almacen/Codigo1/Producto.java
+++ sistema_almacen/Codigo1/Producto.java
@@ -1,1 +1,29 @@
+package Codigo1;
 
+public class Producto {
+    private String sku; 
+    private String nombre;
+    private int cantidad;
+    private double precioUnitario;
+
+    public Producto(String sku, String nombre, int cantidad, double precioUnitario) {
+        this.sku = sku;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public String getSku() { return sku; }
+    public String getNombre() { return nombre; }
+    public int getCantidad() { return cantidad; }
+    public double getPrecio() { return precioUnitario; }
+
+    @Override
+    public String toString() {
+        return "PRODUCTO -> SKU: " + sku + 
+               " | Nombre: " + nombre + 
+               " | Cantidad: " + cantidad + 
+               " | Precio Unit.: S/" + precioUnitario + 
+               " | Subtotal: S/" + (cantidad * precioUnitario);
+    }
+}
