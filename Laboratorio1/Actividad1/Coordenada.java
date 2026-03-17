eab76a965a037186914287e2832ac6b2bd1ad501
Fabrizzio <fbrzzochoa@gmail.com>
2026-03-17 09:21:04 -0500

    Add files via upload

--- Laboratorio1/Actividad1/Coordenada.java
+++ Laboratorio1/Actividad1/Coordenada.java
@@ -1,1 +1,64 @@
+package Actividad1;
 
+import java.util.*; 
+
+public class Coordenada{ 
+private double x; 
+private double y; 
+
+public Coordenada( ){ 
+	this.x = 0;
+	this.y = 0;
+} 
+//Constructor 
+public Coordenada(double x, double y ){ 
+	this.x = x;
+	this.y = y;
+
+} 
+
+//Constructor 
+public Coordenada(Coordenada c ){ 
+	this.x = c.getX();
+	this.y = c.getY();
+ 
+} 
+
+//métodos setter 
+void setX(double  x) { 
+	this.x = x;
+
+} 
+
+void setY(double y){ 
+	this.y=y;
+
+} 
+
+//métodos getter 
+double getX(){ 
+	return this.x;
+     
+} 
+
+double getY(){ 
+    return this.y;
+} 
+
+double distancia(Coordenada c){ 
+    double dif_x = this.x-c.getX();
+    double dif_y = this.y-c.getY();
+    return Math.sqrt(Math.pow(dif_x, 2) + Math.pow(dif_y, 2));
+    
+} 
+
+static double distancia(Coordenada c1, Coordenada c2){ 
+	double dif_x = c1.getX() - c2.getX();
+	double dif_y = c1.getY() - c2.getY();
+	return Math.sqrt(Math.pow(dif_x, 2) + Math.pow(dif_y, 2));
+} 
+@Override
+public String toString() {
+	return "[" + x + "," + y + "]";
+}
+} 
---
+++ Laboratorio1/Actividad1/Principal.java
@@ -0,0 +1,90 @@
+package Actividad1;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Principal {
+
+    public static void mostrarRectangulo(String nombre, Rectangulo r) {
+        System.out.println("Rectangulo " + nombre + " = " + r.toString());
+    }
+
+    public static Rectangulo rectanguloSobre(Rectangulo r1, Rectangulo r2) {
+        double x1_r1 = r1.getEsquina1().getX(); double y1_r1 = r1.getEsquina1().getY();
+        double x1_r2 = r2.getEsquina1().getX(); double y1_r2 = r2.getEsquina1().getY();
+        
+        double x2_r1 = r1.getEsquina2().getX(); double y2_r1 = r1.getEsquina2().getY();
+        double x2_r2 = r2.getEsquina2().getX(); double y2_r2 = r2.getEsquina2().getY();
+
+        double minX = (x1_r1 > x1_r2) ? x1_r1 : x1_r2;
+        double minY = (y1_r1 > y1_r2) ? y1_r1 : y1_r2;
+
+        double maxX = (x2_r1 < x2_r2) ? x2_r1 : x2_r2;
+        double maxY = (y2_r1 < y2_r2) ? y2_r1 : y2_r2;
+
+        return new Rectangulo(new Coordenada(minX, minY), new Coordenada(maxX, maxY));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US); 
+
+        double x1, y1, x2, y2;
+        double x3, y3, x4, y4;
+        boolean esInvalido;
+        do {
+            System.out.println("Ingrese una esquina del 1er rectángulo:");
+            x1 = sc.nextDouble();
+            y1 = sc.nextDouble();
+            System.out.println("Ingrese la esquina opuesta del 1er rectángulo:");
+            x2 = sc.nextDouble();
+            y2 = sc.nextDouble();
+            esInvalido = (x1 == x2) || (y1 == y2);
+
+            if (esInvalido) {
+                System.out.println("Hubo un error con las coordenadas");
+                System.out.println("Ingresa coordenadas validas\n");
+            }
+        } while (esInvalido); 
+
+        do {
+            System.out.println("Ingrese una esquina del 2do rectángulo:");
+            x3 = sc.nextDouble();
+            y3 = sc.nextDouble();
+            System.out.println("Ingrese la esquina opuesta del 2do rectángulo:");
+            x4 = sc.nextDouble();
+            y4 = sc.nextDouble();
+            esInvalido = (x3 == x4) || (y3 == y4);
+
+            if (esInvalido) {
+                System.out.println("Hubo un error con las coordenadas");
+                System.out.println("Ingresa coordenadas validas\\n");
+            }
+        } while (esInvalido);
+
+        Rectangulo rA = new Rectangulo(new Coordenada(x1, y1), new Coordenada(x2, y2));
+        Rectangulo rB = new Rectangulo(new Coordenada(x3, y3), new Coordenada(x4, y4));
+
+        System.out.println("\nResultados");
+
+        mostrarRectangulo("A", rA);
+        mostrarRectangulo("B", rB);
+
+        if (Verificador.esSobrePos(rA, rB)) {
+            System.out.println("Rectangulos A y B se sobreponen.");
+
+            Rectangulo rSobre = rectanguloSobre(rA, rB);
+
+            double areaRedondeada = Math.round(rSobre.calculoArea() * 100.0) / 100.0;
+            System.out.println("Area de sobreposicion = " + areaRedondeada);
+            
+        } else if (Verificador.esJunto(rA, rB)) {
+            System.out.println("Rectangulos A y B se juntan");
+            
+        } else if (Verificador.esDisjunto(rA, rB)) {
+            System.out.println("Rectangulos A y B son disjuntos");
+        }
+
+        sc.close();
+    }
+}
