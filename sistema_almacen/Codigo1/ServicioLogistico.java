cb80067feb70e395bca9471df918844ba47a2c48
samiresp <samiresp.322@gmail.com>
2026-03-16 21:35:12 -0500

    Update ServicioLogistico.java

--- sistema_almacen/Codigo1/ServicioLogistico.java
+++ sistema_almacen/Codigo1/ServicioLogistico.java
@@ -1,1 +1,29 @@
+public class ServicioLogistico {
+    private InventarioListaDoble lista;
 
+    public ServicioLogistico() {
+        this.lista = new InventarioListaDoble();
+    }
+
+    public void registrarEntrada(Producto p) {
+        lista.agregarAlFinal(p);
+        System.out.println("Ssistema, Mercancia registrada bajo SKU " + p.getSku());
+    }
+
+    public void generarReporteValorizacion() {
+        if (lista.estaVacio()) {
+            System.out.println("Inventario en cero");
+            return;
+        }
+        
+        Nodo aux = lista.getCabeza();
+        double inversionTotal = 0;
+        System.out.println("\n REPORTE DE VALORIZACION DE ALMACEN ");
+        while (aux != null) {
+            System.out.println(aux.producto);
+            inversionTotal += (aux.producto.getPrecio() * aux.producto.getCantidad());
+            aux = aux.siguiente;
+        }
+        System.out.println("valor del inventario S/" + inversionTotal);
+    }
+}
