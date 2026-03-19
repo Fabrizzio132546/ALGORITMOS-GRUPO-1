afe956cb0790f7aa1c88744eb32fbbe354b1f8d0
Fabrizzio <fbrzzochoa@gmail.com>
2026-03-18 20:16:47 -0500

    Enhance Rectangulo.java with explanatory comments
    Added comments to clarify the logic for setting corners.

--- Laboratorio1/Actividad1/Rectangulo.java
+++ Laboratorio1/Actividad1/Rectangulo.java
@@ -10,33 +10,33 @@ public class Rectangulo {
     } 
    
     public void setEsquina1(Coordenada coo) { 
-        if (this.esquina2 != null) {
-        	
+        if (this.esquina2 != null) { // verifica que exista la esquina2
+        	// en esta parte se calcula el X y Y minimo para la esquina1
             double minX = (coo.getX() < this.esquina2.getX()) ? coo.getX() : this.esquina2.getX();
             double minY = (coo.getY() < this.esquina2.getY()) ? coo.getY() : this.esquina2.getY();
-            
+         // en esta parte se calcula el X y Y minimo para la esquina2
             double maxX = (coo.getX() > this.esquina2.getX()) ? coo.getX() : this.esquina2.getX();
             double maxY = (coo.getY() > this.esquina2.getY()) ? coo.getY() : this.esquina2.getY();
-            
+            // en esta parte se guarda las coordenadas para sus respectivas esquinas
             this.esquina1 = new Coordenada(minX, minY);
             this.esquina2 = new Coordenada(maxX, maxY);
         } else {
-            this.esquina1 = coo;
+            this.esquina1 = coo;  // en caso que no exista esquina2, solo se guardala coordenada
         }
     } 
- 
     public void setEsquina2(Coordenada coo) { 
-        if (this.esquina1 != null) {
+        if (this.esquina1 != null) { // verifica que exista la esquina 1
+        // en esta parte se busca de igual forma la X y Y minima
             double minX = (this.esquina1.getX() < coo.getX()) ? this.esquina1.getX() : coo.getX();
             double minY = (this.esquina1.getY() < coo.getY()) ? this.esquina1.getY() : coo.getY();
-            
+            // aqui se busca la X y Y maxima
             double maxX = (this.esquina1.getX() > coo.getX()) ? this.esquina1.getX() : coo.getX();
             double maxY = (this.esquina1.getY() > coo.getY()) ? this.esquina1.getY() : coo.getY();
             
             this.esquina1 = new Coordenada(minX, minY);
             this.esquina2 = new Coordenada(maxX, maxY);
         } else {
-            this.esquina2 = coo;
+            this.esquina2 = coo; //En caso que no exista la Esquina1 se guarda la coordeanda
         }
     }
 
@@ -44,12 +44,12 @@ public class Rectangulo {
     public Coordenada getEsquina2() { return this.esquina2; }
     
     @Override
-    public String toString() { 
+    public String toString() {  
         return "(" + this.esquina1.toString() + ", " + this.esquina2.toString() + ")";
     }
     
     public double calculoArea() {
-
+    	// para calular el area debemos encontrar la base y la altura
         double base = this.esquina2.getX() - this.esquina1.getX();
         double altura = this.esquina2.getY() - this.esquina1.getY();
         return base * altura;
