12e1c4fa04962bb9209f6c2b409cbd6c11ea86e5
samiresp <samiresp.322@gmail.com>
2026-03-18 22:31:32 -0500

    Update MayorValor.java
    Comentarios añadidos

--- Laboratorio1/Ejercicio2/MayorValor.java
+++ Laboratorio1/Ejercicio2/MayorValor.java
@@ -1,12 +1,15 @@
 package lab01.ejercicio2;
 
 public class MayorValor {
-	public int fila;
-	public int columna;
-	public double valor;
-	public MayorValor(int fila,int columna,double valor) {
-		this.fila=fila;
-		this.columna=columna;
-		this.valor=valor;
-	}
+    // Ubicación y valor económico de la región óptima
+    public int fila;
+    public int columna;
+    public double valor;
+
+    // Inicializa los datos del hallazgo detectado
+    public MayorValor(int fila, int columna, double valor) {
+        this.fila = fila;
+        this.columna = columna;
+        this.valor = valor;
+    }
 }
