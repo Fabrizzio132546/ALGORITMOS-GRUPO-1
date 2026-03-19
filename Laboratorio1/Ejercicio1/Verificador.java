93762cba2bac2d4947e59d4f0b8a4e80b561d882
TWGus <162067393+TWGus@users.noreply.github.com>
2026-03-18 22:30:13 -0500

    Verificador.java

--- Laboratorio1/Ejercicio1/Verificador.java
+++ Laboratorio1/Ejercicio1/Verificador.java
@@ -3,6 +3,7 @@ package Actividad1;
 public class Verificador {
 
     public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
+        // Verifica si se intersectan en ambos ejes (condición de solapamiento)
         boolean seCruzanEnX = r1.getEsquina1().getX() < r2.getEsquina2().getX() && 
                              r1.getEsquina2().getX() > r2.getEsquina1().getX();
                               
@@ -13,6 +14,7 @@ public class Verificador {
     }
 
     public static boolean esJunto(Rectangulo r1, Rectangulo r2) {
+        // Si se sobreponen, no pueden ser solo "juntos"
         if (esSobrePos(r1, r2)) return false;
 
         boolean chocanBordeX = (r1.getEsquina2().getX() == r2.getEsquina1().getX() || 
@@ -31,6 +33,7 @@ public class Verificador {
     }
 
     public static boolean esDisjunto(Rectangulo r1, Rectangulo r2) {
+        // No se tocan ni se superponen
         return !esSobrePos(r1, r2) && !esJunto(r1, r2);
     }
 }
