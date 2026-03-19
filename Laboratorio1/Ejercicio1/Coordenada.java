1e0533dba056baa8612ba1e59c0f5aa929b5e21a
TWGus <162067393+TWGus@users.noreply.github.com>
2026-03-18 22:29:33 -0500

    Coordenada.java

--- Laboratorio1/Ejercicio1/Coordenada.java
+++ Laboratorio1/Ejercicio1/Coordenada.java
@@ -36,12 +36,14 @@ public class Coordenada {
     } 
 
     double distancia(Coordenada c) { 
+        // Distancia euclidiana entre este punto y otro
         double dif_x = this.x - c.getX();
         double dif_y = this.y - c.getY();
         return Math.sqrt(Math.pow(dif_x, 2) + Math.pow(dif_y, 2));
     } 
 
     static double distancia(Coordenada c1, Coordenada c2) { 
+        // Versión estática del cálculo de distancia
         double dif_x = c1.getX() - c2.getX();
         double dif_y = c1.getY() - c2.getY();
         return Math.sqrt(Math.pow(dif_x, 2) + Math.pow(dif_y, 2));
