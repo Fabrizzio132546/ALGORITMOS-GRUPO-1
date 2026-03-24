d44aa0ee6c1d3e1ae4a08c3cc58d1d9780b7070f
Fabrizzio <fbrzzochoa@gmail.com>
2026-03-24 09:08:46 -0500

    Add Golosina class with basic methods

---
+++ Laboratorio2/Ejercicio1/Golosina.java
@@ -0,0 +1,52 @@
+package Ejercicio1;
+
+public class Golosina {
+
+    private String nombre;
+
+    public Golosina(String nombre, double peso) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {    // verifica si ambos tienen misma direccion 
+            return true; 
+        }
+
+        if (o == null) {     // verifia si el objeto es nulo
+            return false; 
+        }
+        if (o.getClass() != this.getClass()) {  // verifica que el objeto tenga la misma clase 
+            return false; 
+        }
+        Golosina otra = (Golosina) o;   //se aplica casting para que el objeto sea tratado como una golosina
+              
+        
+           // si los dos atributos son nulos entonces son iguales
+        if (this.nombre == null && otra.nombre == null) {
+            return true;
+        } 
+           // si el nombre no es nulo entonces compara con equals q tengan mismo contenido
+        if (this.nombre != null) {
+            return this.nombre.equals(otra.nombre);
+               // retorna true si tienen e mismo contenido
+        }
+
+        return false;
+    }
+}
