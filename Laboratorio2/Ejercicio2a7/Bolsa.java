af0b79c38a0c4179dc0fb60eba88d897899768d6
TWGus <162067393+TWGus@users.noreply.github.com>
2026-03-24 07:11:49 -0500

    Bolsa.java

--- Laboratorio2/Ejercicio2a7/Bolsa.java
+++ Laboratorio2/Ejercicio2a7/Bolsa.java
@@ -1,1 +1,26 @@
+package Actividad01;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bolsa<T> implements Iterable<T> {
+    private ArrayList<T> lista = new ArrayList<>();
+    private int tope;
+
+    public Bolsa(int tope) {
+        this.tope = tope;
+    }
+
+    public void add(T objeto) {
+        if (lista.size() < tope) {
+            lista.add(objeto);
+        } else {
+            throw new RuntimeException("No caben más elementos");
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return lista.iterator();
+    }
+}
