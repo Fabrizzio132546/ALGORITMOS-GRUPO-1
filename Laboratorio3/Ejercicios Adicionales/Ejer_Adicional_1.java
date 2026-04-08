cb696eaa6b633208f3f87eea9c86734e395e4429
Fabrizzio <fbrzzochoa@gmail.com>
2026-04-07 22:42:26 -0500

    Add Heap Sort implementation in EjercicioHeapSort
    Implemented Heap Sort algorithm with heapify function.

--- Laboratorio3/Ejercicios Adicionales/Ejer_Adicional_1.java
+++ Laboratorio3/Ejercicios Adicionales/Ejer_Adicional_1.java
@@ -1,1 +1,46 @@
+package Ejercicios_Adicionales;
+public class EjercicioHeapSort {
 
+    static void heapSort(int[] v) {
+        int n = v.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(v, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = v[0];
+            v[0] = v[i];
+            v[i] = temp;
+
+            heapify(v, i, 0);
+        }
+    }
+
+    static void heapify(int[] v, int n, int i) {
+        int mayor = i;
+        int izq = 2 * i + 1;
+        int der = 2 * i + 2;
+
+        if (izq < n && v[izq] > v[mayor]) mayor = izq;
+        if (der < n && v[der] > v[mayor]) mayor = der;
+
+        if (mayor != i) {
+            int temp = v[i];
+            v[i] = v[mayor];
+            v[mayor] = temp;
+
+            heapify(v, n, mayor);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] v = {12, 11, 13, 5, 6, 7};
+
+        heapSort(v);
+
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i] + " ");
+        }
+    }
+}
