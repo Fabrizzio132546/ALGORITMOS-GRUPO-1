2129d38f6822ee434feb15da4fdb87d033542f7b
Fabrizzio <fbrzzochoa@gmail.com>
2026-05-04 18:56:06 -0500

    añadiendo comentarios de explicacion
    Added comments to clarify the functionality of methods.

--- Laboratorio6/Ejercicios/Ejercicio3/QueueLink.java
+++ Laboratorio6/Ejercicios/Ejercicio3/QueueLink.java
@@ -25,16 +25,19 @@ public class QueueLink<E> implements Queue<E>{
     
     @Override
     public boolean isEmpty() {
-        return inicio == null;
+        return inicio == null; // verificamos si esta vacia
     }
     
     @Override
     public void enqueue(E x) {
+    	// creamos el nuevo nodo
         Node<E> nuevo = new Node<>(x);
         if (isEmpty()) {
+        	// si esta vacio decimos que el inicio y el fin son el nuevo
             inicio = nuevo;
             fin = nuevo;
         } else {
+        	// sino el siguiente de fun es el nuevo y este pasa a ser el fin
             fin.next = nuevo; 
             fin = nuevo;
         }
@@ -45,15 +48,24 @@ public class QueueLink<E> implements Queue<E>{
         if (isEmpty()) {
             throw new ExceptionIsEmpty("la cola esta vacia");
         }
+        // guardamos el dato del inicio
         E d = inicio.data; 
         inicio = inicio.next;  
-        
+        // si el inicio es null, osea que ya no hay mas elementos se pone el fin tambien null
         if (inicio == null) {    
             fin = null;
         }
         return d;
     }
+    
+    public Node<E> getInicio() {
+        return inicio;
+    }
 
+    public Node<E> getFin() {
+        return fin;
+    }
+    // esteme metodo sirve para poder mandar una lista
     public List<E> getElementosComoLista() {
         List<E> lista = new ArrayList<>();
         Node<E> actual = inicio;
