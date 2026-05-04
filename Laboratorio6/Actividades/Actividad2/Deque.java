7f3d1ce27ac52253e909c242cfef2c5edfb4896b
Fabrizzio <fbrzzochoa@gmail.com>
2026-05-03 20:05:36 -0500

    Create Deque.java

---
+++ Laboratorio6/Actividades/Actividad2/Deque.java
@@ -0,0 +1,12 @@
+package Actividad2;
+
+public interface Deque<E> {
+	void addFirst(E x);
+	void addLast(E x);
+	E removeFirst() throws ExceptionIsEmpty;
+	E removeLast() throws ExceptionIsEmpty;
+	E getFirst() throws ExceptionIsEmpty;
+	E getLast() throws ExceptionIsEmpty;
+	boolean isEmpty();
+
+}
