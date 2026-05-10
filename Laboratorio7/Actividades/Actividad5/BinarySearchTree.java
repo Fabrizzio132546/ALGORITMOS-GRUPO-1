c2e696676316108a13d57c5165d741850a3df6b6
Fabrizzio <fbrzzochoa@gmail.com>
2026-05-10 16:10:10 -0500

    Update BinarySearchTree.java

--- Laboratorio7/Actividades/Actividad5/BinarySearchTree.java
+++ Laboratorio7/Actividades/Actividad5/BinarySearchTree.java
@@ -1,1 +1,12 @@
 
+package Actividad5;
+
+import Actividad4.*;
+
+public interface BinarySearchTree<E> {
+	void insert(E data) throws ItemDuplicated;
+	E search(E data) throws ItemNotFound;
+	void delete(E data) throws ExceptionIsEmpty;
+	boolean isEmpty();
+
+}
