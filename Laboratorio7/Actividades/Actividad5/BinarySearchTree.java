43f237750e0ec49f2ef7421438aeb1fa485ff7f0
Fabrizzio <fbrzzochoa@gmail.com>
2026-05-11 20:09:15 -0500

    Update BinarySearchTree.java
    Added methods for inserting, searching, deleting, and checking if the tree is empty.

--- Laboratorio7/Actividades/Actividad5/BinarySearchTree.java
+++ Laboratorio7/Actividades/Actividad5/BinarySearchTree.java
@@ -2,11 +2,15 @@
 package Actividad5;
 
 import Actividad4.*;
-
+// creacion de una interfaz para poder implementarlo en nuestro LinedkBST
 public interface BinarySearchTree<E> {
+	// para insertar un dato
 	void insert(E data) throws ItemDuplicated;
+	// para buscar un dato
 	E search(E data) throws ItemNotFound;
+	// para elimiar un dato
 	void delete(E data) throws ExceptionIsEmpty;
+	// para verificar si esta vacio
 	boolean isEmpty();
 
 }
