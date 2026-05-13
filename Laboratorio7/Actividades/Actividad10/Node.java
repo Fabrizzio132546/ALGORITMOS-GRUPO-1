6521cd19484f94f8d8d06cdb81cfa5b6634209f0
SamirEspinal <samiresp.322@gmail.com>
2026-05-12 22:35:32 -0500

    Update Node.java
    Agregación de Comentarios

--- Laboratorio7/Actividades/Actividad10/Node.java
+++ Laboratorio7/Actividades/Actividad10/Node.java
@@ -2,8 +2,8 @@ package lab07.actividades.actividad10;
 
 class Node<T> {
     T data;
-    Node<T> left;
-    Node<T> right;
+    Node<T> left;  // Puntero al subárbol de elementos menores
+    Node<T> right; // Puntero al subárbol de elementos mayores
 
     public Node(T data) {
         this.data = data;
